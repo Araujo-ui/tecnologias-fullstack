3a9753b6d346421e1b5432ca9b4c0b64885eddfe
Davi Santos Araujo <141648077+Araujo-ui@users.noreply.github.com>
2024-12-02 11:32:26 -0300

    Update OlaMundo.java

--- Java/Fundamentos/OlaMundo.java
+++ Java/Fundamentos/OlaMundo.java
@@ -1,6 +1,5 @@
 public class OlaMundo {
-    public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
-    }
+  public static void main(String[] args) {
+      System.out.println("Olá, Mundo!");
+  }
 }
-
